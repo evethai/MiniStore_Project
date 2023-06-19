41034e47e544527cdf5e22a3a0a7d52a8cd161cd
buiminhquang20 <buiminhquang20@gmail.com>
2023-06-19 12:11:30 +0700

    add new funtion in API_Database

--- Wed/API_Database/Controllers/JWTUtils.cs
+++ Wed/API_Database/Controllers/JWTUtils.cs
@@ -13,7 +13,7 @@ namespace API_Database.Controllers
     {
         private static readonly string SecretKey = "1Y8NuQTyiWqqXCPiwJeCENE23bJE77ydN92cacjb"; // Thay thế bằng khóa bí mật của bạn
 
-        public static string GenerateJWT(EmployeeDTO empdto)
+        public static string GenerateJWTFAcc(EmployeeDTO empdto)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(SecretKey);
@@ -34,6 +34,27 @@ namespace API_Database.Controllers
             return tokenHandler.WriteToken(token);
         }
 
+        public static string GenerateJWTFWS(WorkSheetDTO wsdto)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(SecretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+            new Claim("Sheet", wsdto.Sheet.ToString()),
+            new Claim("TimeCheckIn", wsdto.TimeCheckIn.ToString("MM/dd/yyyy HH:mm:ss")),
+            new Claim("TimeCheckOut", wsdto.TimeCheckOut.ToString("MM/dd/yyyy HH:mm:ss"))
+        }),
+                Expires = DateTime.UtcNow.AddHours(1), // Thời gian hết hạn của JWT: 1 giờ
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+
         public static ClaimsPrincipal ValidateJWT(string jwt)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
@@ -47,7 +68,18 @@ namespace API_Database.Controllers
                 ValidateAudience = false
             };
 
-            return tokenHandler.ValidateToken(jwt, validationParameters, out _);
+            try
+            {
+                SecurityToken validatedToken;
+                return tokenHandler.ValidateToken(jwt, validationParameters, out validatedToken);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi xác minh JWT: " + ex.Message);
+                return null;
+            }
         }
+
+
     }
 }
